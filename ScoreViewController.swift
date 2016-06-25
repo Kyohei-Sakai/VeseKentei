8620ec04109988d3fb77a3b5fd0629d48a34d3f3
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2016-06-25 14:38:24 +0900

    finished app

--- ScoreViewController.swift
+++ ScoreViewController.swift
@@ -6,8 +6,29 @@
 //  Copyright © 2016年 酒井恭平. All rights reserved.
 //
 
-import Cocoa
+//import Cocoa
+import UIKit
 
 class ScoreViewController: UIViewController {
+    
+    //KenteiViewControllerの正解数を受け取るメンバ変数
+    var correct = 0
+    //正解数を表示するラベル
+    @IBOutlet var scoreLabel: UILabel!
+    //合格 or 不合格画像を表示する画像
+    @IBOutlet var judgeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //正解数を表示
+        scoreLabel.text = "正解数は\(correct)もんです。"
+        //合格・不合格を判定
+        if correct >= 7{
+            judgeImageView.image = UIImage(named: "Goukaku.png")
+        }else{
+            judgeImageView.image = UIImage(named: "Fugoukaku.png")
+        }
+    }
+    
 
 }
--- VeseKentei/ViewController.swift
+++ VeseKentei/ViewController.swift
@@ -9,15 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var creditLabel: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //CSVファイル名を引数にしてloadCSVメソッドを使用し、CSVファイルを読み込む
+        let csvArray = loadCSV("start")
+        //print(csvArray)
+        
+        //logoImageViewに画像を設定
+        logoImageView.image = UIImage(named: csvArray[0])
+        //titleLabelにアプリ名を設定
+        titleLabel.text = csvArray[1]
+        //bodyTextViewにアプリ説明文を設定
+        bodyTextView.text = csvArray[2]
+        //ボタンの文字を白色に変更
+        startButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        //creditLabelにクレジットを設定
+        creditLabel.text = csvArray[3]
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //CSVファイルの読み込みメソッド
+    func loadCSV(fileName :String) -> [String]{
+        
+        //CSVファイルのデータを格納するためのString型配列を宣言
+        var csvArray:[String] = []
+        //CSVファイルのパスを設定
+        let csvBundle = NSBundle.mainBundle().pathForResource(fileName, ofType: "csv")!
+        do{
+            //csvBundleからファイルを読み込み、エンコーディングしてcsvDataに格納
+            let csvData = try String(contentsOfFile: csvBundle, encoding: NSUTF8StringEncoding)
+            //改行コードが"\r"の場合は"\n"に置換する
+            let lineChange = csvData.stringByReplacingOccurrencesOfString("\r", withString: "\n")
+            //"\n"の改行コードで要素を切り分け、配列csvArrayに格納する
+            csvArray = lineChange.componentsSeparatedByString("\n")
+            
+        }catch{
+            print("Error!")
+        }
+        
+        return csvArray     //戻り値の配列
     }
 
 
